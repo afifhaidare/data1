a2664306c7794ce87e7f48c5baefe75d8b4812aa
afifhaidare <137806633+afifhaidare@users.noreply.github.com>
2024-03-05 14:05:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns  # Import seaborn for better-looking plots
+import seaborn as sns
 
 # Function to load data (adjust the path as needed)
 @st.cache
